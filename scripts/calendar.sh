c211655583a7f59fa8fd671d34ca1a7c97a503dd
Theo P <theo.park.n@gmail.com>
2023-08-25 10:14:25 -0400

    fix(scripts): fix bug where calendar arithmetic wouldn't work for number greater than 7

--- scripts/calendar.sh
+++ scripts/calendar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 year=$(date +%Y)
-month=$(date +%m)
+# - to produce num without leading 0 which could be interpreted as an octal number
+month=$(date +%-m)
 case $1 in
   'prev') ((month--));;
   'next') ((month++));;
