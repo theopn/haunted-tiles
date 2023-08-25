7dcd45235e5de7e15d880fb442b3b2db6be83fde
Theo P <theo.park.n@gmail.com>
2023-08-25 03:55:01 -0400

    fix(scripts): fix volume control icon brokage in nerd font 3.0

--- scripts/volume-ctrl.sh
+++ scripts/volume-ctrl.sh
@@ -14,11 +14,11 @@ function get_volume_icon() {
   volume=$(get_volume)
   mute=$(get_mute)
   if [ $volume -eq 0 ] || [ $mute == "yes" ] ; then
-    volume_icon="婢"
+    volume_icon="󰖁"
   elif [ $volume -lt 50 ]; then
-    volume_icon=""
+    volume_icon="󰖀"
   else
-    volume_icon=""
+    volume_icon="󰕾"
   fi
 }
 
