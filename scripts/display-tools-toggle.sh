c776b42aa8c09129a2ab57a31eb4d5b2f611d283
Theo P <theo.park.n@gmail.com>
2025-05-24 06:10:41 -0400

    refactor(polybar): simplify extra menu

--- scripts/display-tools-toggle.sh
+++ scripts/display-tools-toggle.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
 
-case $1 in
-  redshift)
-    if [[ $(ps -aux | grep [r]edshift | wc -l) > 0 ]]; then
-      redshift -x
-      pkill -USR1 redshift
-    else
-      pkill -USR1 redshift
-      redshift -P -l 39.2:-86.5 -t 5600:3500 -m randr
-    fi
-    ;;
-  compositor)
-    if [[ $(ps -aux | grep [p]icom | wc -l) > 0 ]]; then
-      pkill -9 picom
-    else
-      picom -b
-    fi
-    ;;
-  gammastep)
-    if [[ $(ps -aux | grep [g]ammastep | wc -l) > 0 ]]; then
-      gammastep -x &
-      pkill -9 gammastep &
-      gammastep -x
-    else
-      gammastep -P -l 39.2:-86.5 -t 5600:3500
-    fi
-    ;;
-  *)
-    echo 'Invalid option'
-    exit 1;
-esac
-
-exit 0;
+if [[ $(ps -aux | grep [r]edshift | wc -l) > 0 ]]; then
+  redshift -x
+  sleep 1
+  pkill -USR1 redshift
+else
+  redshift -x
+  pkill -USR1 redshift
+  sleep 1
+  redshift -P
+fi
 
