f2fe5406206a268f0586351f3e6ee3871c384378
Theo P <theo.park.n@gmail.com>
2025-05-30 12:13:45 -0400

    feat: add redshift and gammastep config generator to the config

--- install.sh
+++ install.sh
@@ -13,8 +13,34 @@ feh --bg-fill $XDG_CONFIG_HOME/wallpaper.png
 ln -s ${HAUNTED_TILES_DIR}/polybar ${XDG_CONFIG_HOME}/polybar
 ln -s ${HAUNTED_TILES_DIR}/picom ${XDG_CONFIG_HOME}/picom
 
+cat <<EOF >> ${XDG_CONFIG_HOME}/redshift.conf
+[redshift]
+temp-day=5600
+temp-night=3500
+gamma=0.8
+adjustment-method=randr
+location-provider=manual
+
+[manual]
+lat=0.0
+lon=0.0
+EOF
+
 echo "Deploying Sway, Swaylock, and Waybar..."
 ln -s ${HAUNTED_TILES_DIR}/sway ${XDG_CONFIG_HOME}/sway
 ln -s ${HAUNTED_TILES_DIR}/swaylock ${XDG_CONFIG_HOME}/swaylock
 ln -s ${HAUNTED_TILES_DIR}/waybar ${XDG_CONFIG_HOME}/waybar
 
+mkdir -p ${XDG_CONFIG_HOME}/gammastep
+cat <<EOF >> ${XDG_CONFIG_HOME}/gammastep/config.ini
+[general]
+temp-day=5600
+temp-night=3500
+gamma=0.8
+adjustment-method=wayland
+location-provider=manual
+
+[manual]
+lat=0.0
+lon=0.0
+EOF
