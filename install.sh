4d21afe6ebdd0f05dbb31c5e184b875ae8e37970
Theo P <theo.park.n@gmail.com>
2026-02-02 02:06:07 -0500

    feat(gammastep): add gammastep config that uses time-based shift

--- install.sh
+++ install.sh
@@ -8,20 +8,7 @@ ln -s "${HAUNTED_TILES_DIR}/rofi"       "${XDG_CONFIG_HOME}/rofi"
 ln -s "${HAUNTED_TILES_DIR}/sway"       "${XDG_CONFIG_HOME}/sway"
 ln -s "${HAUNTED_TILES_DIR}/swaylock"   "${XDG_CONFIG_HOME}/swaylock"
 ln -s "${HAUNTED_TILES_DIR}/waybar"     "${XDG_CONFIG_HOME}/waybar"
-
-mkdir -p "${XDG_CONFIG_HOME}/gammastep"
-cat <<EOF >> "${XDG_CONFIG_HOME}/gammastep/config.ini"
-[general]
-temp-day=5600
-temp-night=3500
-gamma=0.8
-adjustment-method=wayland
-location-provider=manual
-
-[manual]
-lat=0.0
-lon=0.0
-EOF
+ln -s "${HAUNTED_TILES_DIR}/gammastep"  "${XDG_CONFIG_HOME}/gammastep"
 
 mkdir -p ~/.local/bin
 for f in "${HAUNTED_TILES_DIR}/scripts/*"; do
