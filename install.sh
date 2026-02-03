c62f41376f0bcb92a103d2f4e795a2f9a6cef797
Theo P <theo.park.n@gmail.com>
2026-02-02 20:13:54 -0500

    feat(sayidle): add swayidle config instead of handling in compositor level

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ HAUNTED_TILES_DIR=${XDG_CONFIG_HOME}/haunted-tiles
 ln -s "${HAUNTED_TILES_DIR}/dunst"      "${XDG_CONFIG_HOME}/dunst"
 ln -s "${HAUNTED_TILES_DIR}/rofi"       "${XDG_CONFIG_HOME}/rofi"
 ln -s "${HAUNTED_TILES_DIR}/sway"       "${XDG_CONFIG_HOME}/sway"
+ln -s "${HAUNTED_TILES_DIR}/swayidle"   "${XDG_CONFIG_HOME}/swayidle"
 ln -s "${HAUNTED_TILES_DIR}/swaylock"   "${XDG_CONFIG_HOME}/swaylock"
 ln -s "${HAUNTED_TILES_DIR}/waybar"     "${XDG_CONFIG_HOME}/waybar"
 ln -s "${HAUNTED_TILES_DIR}/gammastep"  "${XDG_CONFIG_HOME}/gammastep"
